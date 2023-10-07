3ef2e7e78e4787b30a067c6eeb6743a167dab3ab
wikicious <49991364+hetwik@users.noreply.github.com>
2023-10-07 21:55:58 +0530

    service worker
    service worker

--- next.config.js
+++ next.config.js
@@ -70,3 +70,15 @@ module.exports = withSentryConfig(
     disableLogger: true,
   },
 )
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/',
+  sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  // next.js config
+})
