287f2b8ee0cb7506b2108ca9107f5a10d2562a8b
wikicious <49991364+hetwik@users.noreply.github.com>
2023-10-07 21:43:30 +0530

    revert
    revert

--- next.config.js
+++ next.config.js
@@ -70,16 +70,3 @@ module.exports = withSentryConfig(
     disableLogger: true,
   },
 )
-
-const withPWA = require('next-pwa')({
-  dest: 'public'
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-})
-
-module.exports = withPWA({
-  // next.js config
-})
