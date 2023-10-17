dee31172db7afd61045679e4f50d3eec0040918c
Arpit Verma <arpitv970@gmail.com>
2023-10-17 11:36:17 +0530

    updated .gitignore

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
         <Head>
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <meta name="theme-color" content="#317EFB" />
           <Script
             src="/datafeeds/udf/dist/bundle.js"
             strategy="beforeInteractive"
